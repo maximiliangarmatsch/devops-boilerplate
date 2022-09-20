e7085e9bc9ff883f4216e9d4c21c72af182ca94c
zt4ff <segunkayode00@gmail.com>
2022-09-20 10:09:15 +0100

    init: lighthouse ci checks with a minimum score of 0.1 on www.github.com

---
+++ lighthouserc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ["https://www.github.com"],
+      assert: {
+        assertions: {
+          "categories:performance": ["warn", { minScore: 0.1 }],
+          "categories:accessibility": ["warn", { minScore: 0.1 }],
+          "categories:seo": ["warn", { minScore: 0.1 }],
+          "categories:best-practices": ["warn", { minScore: 0.1 }],
+        },
+      },
+    },
+  },
+};
